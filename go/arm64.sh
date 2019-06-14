40cd26bc4cbb36756579fd64f8b1c4529607830c
- <->
2019-06-14 16:42:01 +0800

    .

---
+++ go/arm64.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+_dirname=$(cd "$(dirname "$0")"; pwd)
+
+cd $_dirname
+
+CC=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android28-clang  GOOS=android GOARCH=arm64 CGO_ENABLED=1 go build main.go
