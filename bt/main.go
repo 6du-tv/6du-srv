8490c183ab1c10c84f87d335276c06d036e1f84b
- <->
2019-06-17 14:13:03 +0800

    .

--- bt/main.go
+++ bt/main.go
@@ -44,7 +44,7 @@ func main() {
 	for scanner.Scan() {
 		var uri string = scanner.Text()
 		if len(uri) > 0 {
-			wait.Add(func() { connect(uri) })
+			wait.Call(func() { connect(uri) })
 		}
 	}
 	wait.Wait()
