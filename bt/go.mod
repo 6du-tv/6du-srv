03fa32e5ac5cb07bd97ed4974fe4332df3d076b9
- <->
2019-06-18 17:50:20 +0800

    .

--- bt/go.mod
+++ bt/go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/RoaringBitmap/roaring v0.4.17
+	github.com/deckarep/golang-set v1.7.1
 	github.com/urwork/throw v0.0.0-20190618055048-7c9a09a1ea0f
 )
--- bt/udp/parse.go
+++ bt/udp/parse.go
@@ -3,7 +3,23 @@ package udp
 import (
 	"fmt"
 	"net"
+	"time"
+
+	mapset "github.com/deckarep/golang-set"
 )
+
+var REPLYD = mapset.NewSet()
+
+func init() {
+	ticker := time.NewTicker(1 * time.Second)
+
+	go func() {
+		// write a message to server
+		for range ticker.C {
+			REPLYD.Clear()
+		}
+	}()
+}
 
 func Parse(buf []byte, remote *net.UDPAddr, conn *Conn) {
 	cmd := CMD(buf[0])
@@ -11,6 +27,7 @@ func Parse(buf []byte, remote *net.UDPAddr, conn *Conn) {
 	switch cmd {
 
 	case PING:
+		print("REPLYD ", REPLYD.Cardinality())
 		conn.WriteUDP([]byte{byte(PONG)}, remote)
 	default:
 		fmt.Printf("<<<  %d bytes received from: %v, data: %x\n", len(buf), remote, buf)
