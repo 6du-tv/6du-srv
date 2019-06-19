157b9bc0cd2f19d52ca4847bb23dc2701e660fa1
- <->
2019-06-19 13:18:42 +0800

    .

--- bt/main.go
+++ bt/main.go
@@ -22,7 +22,7 @@ import (
 */
 
 func main() {
-	fmt.Printf("%x", KEY)
+	fmt.Printf("%x", KEY.SECRET)
 	localAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", CONFIG.NET.PORT))
 	Throw(err)
 
